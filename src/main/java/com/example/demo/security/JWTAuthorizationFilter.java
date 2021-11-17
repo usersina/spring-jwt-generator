f35b177c7866a3f9895eabc5afd5f9aae6ce8233
UserSina <benna.anis2@gmail.com>
2021-11-17 22:36:52 +0100

    Add JWT authorization filter

---
+++ src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
@@ -0,0 +1,51 @@
+package com.example.demo.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String jwt = request.getHeader("Authorization");
+		if(jwt == null || !jwt.startsWith(SecurityParams.PREFIX)) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SecurityParams.SECRET)).build();
+		jwt = jwt.substring(SecurityParams.PREFIX.length());
+		
+		DecodedJWT decodedJWT = verifier.verify(jwt);	
+		String username = decodedJWT.getSubject();
+		List<String> roles = decodedJWT.getClaims().get("roles").asList(String.class);
+	
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		for(String r: roles)
+			authorities.add(new SimpleGrantedAuthority(r));
+		
+		UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(username, roles, authorities);
+		SecurityContextHolder.getContext().setAuthentication(user);
+		filterChain.doFilter(request, response);
+	}
+}
--- src/main/java/com/example/demo/security/SecurityParams.java
+++ src/main/java/com/example/demo/security/SecurityParams.java
@@ -3,4 +3,6 @@ package com.example.demo.security;
 public interface SecurityParams {
 	public static final long EXP_TIME = 10*24*60*60*1000;
 	public static final String SECRET = "my&super&secret!";
+	public static final String PREFIX = "Bearer ";
+
 }
