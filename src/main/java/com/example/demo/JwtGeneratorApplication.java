08813a326edda7a5e2ccd8566a970c17e97cd4e2
usersina <benna.anis2@gmail.com>
2021-11-21 17:46:10 +0100

    Change app port

--- src/main/java/com/example/demo/JwtGeneratorApplication.java
+++ src/main/java/com/example/demo/JwtGeneratorApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import javax.annotation.PostConstruct;
+// import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -8,16 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.example.demo.entities.Role;
-import com.example.demo.entities.User;
+// import com.example.demo.entities.Role;
+// import com.example.demo.entities.User;
 import com.example.demo.service.UserService;
 
 @SpringBootApplication
 public class JwtGeneratorApplication {
-	
+
 	@Autowired
 	UserService userService;
-	
+
 	@Bean
 	BCryptPasswordEncoder getBCE() {
 		return new BCryptPasswordEncoder();
@@ -26,23 +26,23 @@ public class JwtGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtGeneratorApplication.class, args);
 	}
-	
-	/*@PostConstruct
-	void init_users() {
-		// Creating roles
-		userService.addRole(new Role(null, "ADMIN"));
-		userService.addRole(new Role(null, "USER"));
-		
-		// Creating users
-		userService.saveUser(new User(null, "Admin", "123", true, null));
-		userService.saveUser(new User(null, "Sina", "123", true, null));
-		userService.saveUser(new User(null, "John", "123", true, null));
-		
-		// Adding roles to users
-		userService.addRoleToUser("Admin", "ADMIN");
-		userService.addRoleToUser("Admin", "USER");
-		userService.addRoleToUser("Sina", "USER");
-		userService.addRoleToUser("John", "USER");
-	}*/
+
+	// @PostConstruct
+	// void init_users() {
+	// // Creating roles
+	// userService.addRole(new Role(null, "ADMIN"));
+	// userService.addRole(new Role(null, "USER"));
+
+	// // Creating users
+	// userService.saveUser(new User(null, "Admin", "123", true, null));
+	// userService.saveUser(new User(null, "Sina", "123", true, null));
+	// userService.saveUser(new User(null, "John", "123", true, null));
+
+	// // Adding roles to users
+	// userService.addRoleToUser("Admin", "ADMIN");
+	// userService.addRoleToUser("Admin", "USER");
+	// userService.addRoleToUser("Sina", "USER");
+	// userService.addRoleToUser("John", "USER");
+	// }
 
 }
