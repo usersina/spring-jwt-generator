0bec55842bee3b6528b88a1d18a0c2183e912f8c
usersina <benna.anis2@gmail.com>
2021-12-07 20:53:33 +0100

    Remove extra import

--- src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
+++ src/main/java/com/example/demo/security/JWTAuthorizationFilter.java
@@ -3,7 +3,6 @@ package com.example.demo.security;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.FilterChain;
